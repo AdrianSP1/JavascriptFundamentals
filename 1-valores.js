c7688d60653f47bdfffffc19385269e702a1a9df
xiod <adriangustavose@gmail.com>
2021-09-22 11:04:23 -0300

    firstcommit-valoresjs

---
+++ 1-valores.js
@@ -0,0 +1,19 @@
+// Tipos primitivos
+
+33 // Numeros
+3.14
+"Esto es un String(cadena de texto)" //Strings (cadena de texto), Esto es la forma en la que puedo crear algo que se pueda imprimir en la pantalla, o escribir información, que el usuario pueda entender y leer. utilizar palabras, o frases
+
+'wubba lubba dub dub'
+true //Booleanos, estos fueron inventado por George Boole, un Matemático británico. y esto hace referenci a cosas que podamos validar.
+false
+
+null //Valores vacios(Empty values), es como un placeholder(espacio reservado para algun valor) de algun valor faltante.
+
+undefined // Valor asignado automaticamente a las variables que se declararon o a los argumentos formales para los cuales no existe argumentos reales.
+
+/* ================================================================ */
+
+//Tipos de objetos o no primitivos
+[1, 2, 3, 4] // corchetes // arrays
+{nombre:'Adrian'} // llaves // objetos
