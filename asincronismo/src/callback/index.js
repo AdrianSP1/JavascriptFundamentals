1765c8dbdf5b1ccfe16968ca367f3def97cdc467
xiod <adriangustavose@gmail.com>
2021-10-13 20:54:28 -0300

    async+date*2

--- asincronismo/src/callback/index.js
+++ asincronismo/src/callback/index.js
@@ -6,4 +6,20 @@ function calc(num1, num2, callback){
     return callback(num1, num2);
 }
 
-console.log(calc(2, 2, sum));
+console.log(calc(85, 6, sum));
+
+function date(callback){
+    console.log(new Date);
+    setTimeout(function(){
+        let date = new Date;
+        callback(date);
+    }, 3000)
+
+}
+
+function printDate(dateNow){
+    console.log(dateNow);
+}
+
+date(printDate);
+
