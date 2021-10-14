99369e2a550e93cfa9dd7abc67b302e808dae012
xiod <adriangustavose@gmail.com>
2021-10-14 09:26:36 -0300

    callback-API-rickandmorty

--- asincronismo/src/callback/index.js
+++ asincronismo/src/callback/index.js
@@ -1,14 +1,15 @@
+//funcion suma de dos elementos
 function sum(num1, num2){
     return num1 + num2;
 }
-
+//funcion que recibe como argumento la funcion sum callback
 function calc(num1, num2, callback){
     return callback(num1, num2);
 }
 
-console.log(calc(85, 6, sum));
+console.log(calc(85, 6, sum)); //ejecuto la funcion calc con los dos parametros y los suma
 
-function date(callback){
+function date(callback){//callback que muestra la fecha con 3 segundos de diferencia setTimeout 3000
     console.log(new Date);
     setTimeout(function(){
         let date = new Date;
@@ -17,9 +18,9 @@ function date(callback){
 
 }
 
-function printDate(dateNow){
+function printDate(dateNow){// funcion que imprime la fecha actual
     console.log(dateNow);
 }
 
-date(printDate);
+date(printDate)
 
